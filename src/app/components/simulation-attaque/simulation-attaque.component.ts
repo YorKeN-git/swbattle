16bd2ea70f7ceb6520ca3da7809d7a06f50f69a4
YorKeN <dethoor.axel@gmail.com>
2020-04-28 16:10:59 +0200

    WIP : entrainement

--- src/app/components/simulation-attaque/simulation-attaque.component.ts
+++ src/app/components/simulation-attaque/simulation-attaque.component.ts
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SimulationAttaqueComponent implements OnInit {
 
+  //Boolean pour la création du Héro d'entrainement
+  choixClasseFait: boolean = false;
+  choixArmeFait: boolean = false;
+  epee: boolean = false;
+  masse: boolean = false;
+  dague: boolean = false;
+  arc: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  entrainementChoixClasse(choixClasse: string){
+    //Affiche le choix de l'arme en fonction de la classe selectionnée
+    this.choixClasseFait = true;
+    switch (choixClasse) {
+      case 'guerrier':
+        this.epee = true;
+        this.masse = true;
+        this.dague = false;
+        this.arc = false;
+        break;
+      case 'soigneur':
+        this.epee = true;
+        this.masse = true;
+        this.dague = false;
+        this.arc = false;
+        break;
+      case 'assassin':
+        this.epee = true;
+        this.masse = false;
+        this.dague = true;
+        this.arc = false;
+        break;
+      case 'archer':
+        this.epee = false;
+        this.masse = false;
+        this.dague = false;
+        this.arc = true;
+        break;
+      default:
+        break;
+    }
+  }
+
+  entrainementChoixArme(){
+    this.choixArmeFait = true;
   }
 
 }
